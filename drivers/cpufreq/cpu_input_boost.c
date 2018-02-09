bb38f0a0eda50a4606f48abbf6d20a2968ea8543
joshuous <joshuous@gmail.com>
2018-02-09 22:52:10 +0800

    cpu_input_boost: Implement Dynamic SchedTune Boost v3
    Dynamic SchedTune Boost occurs on-the-fly during interactions, such
    as touch input. This idea was conceived because of an existing battery
    drain issue whereby setting a permanent schedtune boost would cause
    boosting to occur even when the phone was idling. For example, if a user
    sets the /dev/stune/top-app/schedtune.boost to 10, the device may not
    idle at the lowest frequency step, which can lead to higher idle and
    active drain rates. Thus it should consume less power to boost only
    during interactions.
    
    v1 worked by changing the value returned by schedtune_cpu_boost(), but
    did not change the true boost value of the top-app cgroup. In comparison,
    v2 modifies the value in /dev/stune/top-app/schedtune.boost directly.
    v3 reworks the entire Dynamic SchedTune Boost code and improves detection
    of top-app or any other SchedTune group.
    
    The tunable can be found in /sys/module/cpu_boost/parameters/dynamic_stune_boost.
    
    Signed-off-by: joshuous <joshuous@gmail.com>
    [nc: Adapted from cpu-boost.c to cpu_input_boost.c]
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: LibXZR <xzr467706992@163.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -81,6 +81,11 @@ static void __cpu_input_boost_kick(struct boost_drv *b)
 		return;
 
 	set_bit(INPUT_BOOST, &b->state);
+
+	#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+	do_stune_boost("top-app", dynamic_stune_boost);
+	#endif
+
 	if (!mod_delayed_work(system_unbound_wq, &b->input_unboost,
 			      msecs_to_jiffies(CONFIG_INPUT_BOOST_DURATION_MS)))
 		wake_up(&b->boost_waitq);
@@ -122,6 +127,10 @@ void cpu_input_boost_kick_max(unsigned int duration_ms)
 {
 	struct boost_drv *b = &boost_drv_g;
 
+	#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+	do_stune_boost("top-app", dynamic_stune_boost);
+	#endif
+
 	__cpu_input_boost_kick_max(b, duration_ms);
 }
 
@@ -132,6 +141,10 @@ static void input_unboost_worker(struct work_struct *work)
 
 	clear_bit(INPUT_BOOST, &b->state);
 	wake_up(&b->boost_waitq);
+
+	#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+	reset_stune_boost("top-app");
+	#endif
 }
 
 static void max_unboost_worker(struct work_struct *work)
@@ -141,6 +154,10 @@ static void max_unboost_worker(struct work_struct *work)
 
 	clear_bit(MAX_BOOST, &b->state);
 	wake_up(&b->boost_waitq);
+
+	#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+	reset_stune_boost("top-app");
+	#endif
 }
 
 static int cpu_boost_thread(void *data)
@@ -269,6 +286,10 @@ free_handle:
 
 static void cpu_input_boost_input_disconnect(struct input_handle *handle)
 {
+
+	#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+	reset_stune_boost("top-app");
+	#endif
 	input_close_device(handle);
 	input_unregister_handle(handle);
 	kfree(handle);
