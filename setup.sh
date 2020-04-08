294e542fb9ada65a4c43053b307a1edb44e72495
Ratoriku <a1063021545@gmail.com>
2020-04-08 08:01:29 +0800

    setup.sh: Update
    Signed-off-by: Ratoriku <a1063021545@gmail.com>

--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ cd ..
 mkdir cbl && cd cbl
 wget https://dl.akr-developers.com/s/CBL/LiuNian_clang-20200402-1.tar.zst
 tar -I zstd -xvf *.zst
-echo "Note: Be sure yours glib version>=2.30"
+echo "Note: Be sure your glib version>=2.30"
+ldd --version
