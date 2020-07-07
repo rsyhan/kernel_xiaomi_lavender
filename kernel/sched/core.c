4449576f76c01c760091840271229c7f2046d38f
Ratoriku <a1063021545@gmail.com>
2020-07-07 10:23:21 +0800

    Revert "sched/core: Remove unlikely() annotation from sched_move_task()"
    This reverts commit 6111236aeb0baaa33a0374f555c540b9f58d0ed1.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8505,12 +8505,12 @@ void sched_move_task(struct task_struct *tsk)
 
 	if (queued)
 		dequeue_task(rq, tsk, DEQUEUE_SAVE | DEQUEUE_MOVE);
-	if (running)
+	if (unlikely(running))
 		put_prev_task(rq, tsk);
 
 	sched_change_group(tsk, TASK_MOVE_GROUP);
 
-	if (running)
+	if (unlikely(running))
 		tsk->sched_class->set_curr_task(rq);
 	if (queued)
 		enqueue_task(rq, tsk, ENQUEUE_RESTORE | ENQUEUE_MOVE);
